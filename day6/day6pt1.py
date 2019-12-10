60dbbd0bbc196d3e700aa27ffd78e981b2597222
Na-Nazhou <nazhou.na9@gmail.com>
2019-12-10 14:20:10 +0800

    Day 6 Part 2

--- day6/day6pt1.py
+++ day6/day6pt1.py
@@ -4,22 +4,22 @@ import sys
 
 
 def main():
-    graph = {}
+    p = {}
     total = 0
     for edge in sys.stdin:
         vertices = edge.strip().split(")")
         v1 = vertices[0]
         v2 = vertices[1]
-        if v1 not in graph.keys():
-            graph[v1] = None
-        graph[v2] = v1
+        if v1 not in p.keys():
+            p[v1] = None
+        p[v2] = v1
 
-    for child, parent in graph.items():
+    for child, parent in p.items():
         count = 0
         curr = parent
         while curr:
             count += 1
-            curr = graph[curr]
+            curr = p[curr]
         total += count
     print(total)  # output
 
--- day6/day6pt2.py
+++ day6/day6pt2.py
@@ -1,10 +1,50 @@
 #!/usr/bin/env python3
 
 import sys
+import queue
+
+
+def add_neighbor(v1, v2, graph):
+    if v1 not in graph.keys():
+        graph[v1] = []
+    graph[v1].append(v2)
+
+
+def bfs(graph, src, dest):
+    p = {}
+    q = queue.Queue()
+    q.put(src)
+    p[src] = None
+    while not q.empty():
+        u = q.get()
+        for neighbor in graph[u]:
+            if neighbor not in p.keys():
+                p[neighbor] = u
+                q.put(neighbor)
+        if dest in p.keys():
+            break
+
+    count = 0
+    curr = dest
+    while curr != src:
+        count += 1
+        curr = p[curr]
+    return count
 
 
 def main():
-    pass
+    graph = {}
+    for edge in sys.stdin:
+        vertices = edge.strip().split(")")
+        v1 = vertices[0]
+        v2 = vertices[1]
+        add_neighbor(v1, v2, graph)
+        add_neighbor(v2, v1, graph)
+        if v2 == 'YOU':
+            src = v1
+        elif v2 == 'SAN':
+            dest = v1
+    print(bfs(graph, src, dest))
 
 
 if __name__ == "__main__":
