2246085a7fec3bde2a9ba84529f6f80a6f8cb6a5
Na-Nazhou <nazhou.na9@gmail.com>
2019-12-11 16:22:49 +0800

    Day 10 Part 2

--- day10/day10pt2.py
+++ day10/day10pt2.py
@@ -43,11 +43,19 @@ def main():
     max_detection = 0
     for asteroid in asteroids:
         count = 0
+        visible = {}
         for other in asteroids:
             if other != asteroid and check(asteroid, other, asteroids):
                 count += 1
-        max_detection = max(count, max_detection)
-    print(max_detection)  # output
+                visible[other] = get_degree(asteroid, other)
+        if count > max_detection:
+            max_detection = count
+            monitored = visible
+
+    sorted_asteroids = sorted(monitored.items(), key=lambda item: item[1])
+    # Assumption: there are at least 200 asteroids that can be detected
+    the200th = sorted_asteroids[199][0]
+    print(the200th[0] * 100 + the200th[1])  # output
 
 
 if __name__ == "__main__":
