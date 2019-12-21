809eca3a4e800323bf50b5b3418e4d66e63d252d
Na-Nazhou <nazhou.na9@gmail.com>
2019-12-21 14:56:38 +0800

    Day 13 Part 2

--- day13pt2.py
+++ day13pt2.py
@@ -4,37 +4,31 @@ from intcode.IntCodeComputer import IntCodeComputer
 
 def main():
     memory = read()
-    # memory[0] = 2
+    memory[0] = 2
     intcode = IntCodeComputer(memory)
-    screen = {}
-    min_x = 10e9
-    min_y = 10e9
-    max_x = -10e9
-    max_y = -10e9
+    ball_x = -1
+    paddle_x = -1
+    score = -1
     while not intcode.completed:
         intcode.run()
         while not intcode.output.empty():
             x = intcode.output.get()
             y = intcode.output.get()
             tile_id = intcode.output.get()
-            screen[(x, y)] = tile_id
-            min_x = min(min_x, x)
-            min_y = min(min_y, y)
-            max_x = max(max_x, x)
-            max_y = max(max_y, y)
-    for j in range(min_y, max_y + 1):
-        for i in range(min_x, max_x + 1):
-            if screen[(i, j)] == 0:
-                print(" ", end="")
-            elif screen[(i, j)] == 1:
-                print("w", end="")
-            elif screen[(i, j)] == 2:
-                print("b", end="")
-            elif screen[(i, j)] == 3:
-                print("_", end="")
-            elif screen[(i, j)] == 4:
-                print("*", end="")
-        print()
+            if x == -1 and y == 0:
+                score = tile_id
+            if tile_id == 3:
+                paddle_x = x
+            elif tile_id == 4:
+                ball_x = x
+        if paddle_x < ball_x:
+            ipt = 1
+        elif paddle_x > ball_x:
+            ipt = -1
+        else:
+            ipt = 0
+        intcode.read(ipt)
+    print(score)
 
 
 if __name__ == "__main__":
